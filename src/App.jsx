ad11fd60b11cd05cbb53080af381bead3c5c3a71
shamonicaJ <shamonica.jones@gmail.com>
2024-01-26 14:43:52 -0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
-import { useState } from 'react'
-import bookLogo from './assets/books.png'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Navigations from './components/Navigations'
+import Books from './components/books/Books'
+import BookDetails from './components/books/BookDetails'
+import Register from './components/account/Register'
+import Account from './components/account/Account'
+import Login from './components/account/Login'
+import { useSelector } from 'react-redux'
 
 function App() {
-  const [token, setToken] = useState(null)
+  const token = useSelector(state => state.auth.token)
 
   return (
     <>
-      <h1><img id='logo-image' src={bookLogo}/>Library App</h1>
-
-      <p>Complete the React components needed to allow users to browse a library catalog, check out books, review their account, and return books that they've finished reading.</p>
-
-      <p>You may need to use the `token` in this top-level component in other components that need to know if a user has logged in or not.</p>
-
-      <p>Don't forget to set up React Router to navigate between the different views of your single page application!</p>
+      <Navigations />
+      <Routes>
+        <Route path="/" element={<Navigate to="/books" />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/books/:id" element={<BookDetails />} />
+        { token ? (
+          <>
+            <Route path="/account" element={<Account />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </>
+        )}
+      </Routes>
     </>
   )
 }
 
 export default App
+
